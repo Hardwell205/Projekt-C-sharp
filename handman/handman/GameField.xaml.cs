76b86977da82b2ad647d399897e7af3a918543bb
Ozyrus26 <56609412+Ozyrus26@users.noreply.github.com>
2020-01-30 08:02:04 +0100

    hangman

---
+++ handman/handman/GameField.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace handman
+{
+    /// <summary>
+    /// Logika interakcji dla klasy GameField.xaml
+    /// </summary>
+    public partial class GameField : Window
+    {
+        
+        public GameField()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private int wrongLetter = 0;
+        private string haslo = "";
+        private string kopiaHaslo = "";
+        string komunikat;
+        private string[] words;
+
+        private void attempt(object sender, EventArgs e)
+        {
+            wrongLetter++;
+            if (wrongLetter > 11)
+            {
+                Console.WriteLine('Przegrałeś!');
+            }
+        }
+        private void loadwords()
+        {
+            char[] delimiterChars = { ',' };
+            string[] readText = File.ReadAllLines('hasla.csv');
+            words = new string[readText.Length];
+            int index = 0;
+            foreach (string s in readText)
+            {
+                string[] line = s.Split(delimiterChars);
+
+                words[index++] = line[1];
+            }
+            int end = 0;
+        }
+        private void randomChoice()
+        {
+            wrongLetter = 0;
+            int rollInx = (new Random()).Next(words.Length);
+            haslo = words[rollInx];
+            kopiaHaslo = "";
+            for (int index = 0; index < kopiaHaslo.Length; index++)
+            {
+                kopiaHaslo += "_"
+            }
+            printCopy();
+
+        }
+        private void printCopy()
+        {
+            for (int index = 0; index < kopiaHaslo.Length; index++)
+            {
+                lblShowWord.Text += kopiaHaslo.Substring(index, 1) += "_";
+                lblShowWord.Text += " ";
+            }
+        }
+        private void overRideCody(char guess)
+        {
+
+        }
+
+    }
+}
+}
---
+++ handman/handman/Library.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace handman
+{
+    class Library
+    {
+       
+        private int wrongLetter = 0;
+        private string haslo = "";
+        private string kopiaHaslo = "";
+        string komunikat;
+        private string[] words;
+
+        private void attempt(object sender, EventArgs e)
+        {
+            wrongLetter++;
+            if(wrongLetter > 11)
+            {
+                komunikat = 'Przegrałeś!';
+            }
+        }
+        private void loadwords()
+        {
+            char[] delimiterChars = { ',' };
+            string[] readText = File.ReadAllLines('hasla.csv');
+            words = new string[readText.Length];
+            int index = 0;
+            foreach (string s in readText)
+            {
+                string[] line= s.Split(delimiterChars);
+
+                words[index++] = line[1];
+            }
+            int end = 0;
+        }
+        private void randomChoice()
+        {
+            wrongLetter = 0;
+            int rollInx = (new Random()).Next(words.Length);
+            haslo = words[rollInx];
+            kopiaHaslo = "";
+            for( int index = 0; index <kopiaHaslo.Length; index++)
+            {
+                kopiaHaslo += "_"
+            }
+            printCopy();
+            
+        }
+        private void printCopy()
+        {
+            for (int index = 0; index < kopiaHaslo.Length; index++)
+            {
+                lblShowWord.Text += kopiaHaslo.Substring(index,1) += "_";
+                lblShowWord.Text += " ";
+            }
+        }
+        private void overRideCody(char guess)
+        {
+
+        }
+        
+    }
+}
